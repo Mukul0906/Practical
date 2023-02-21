e8ec4c4a4fe9996682e428478468d735fc4f9ecc
Mukul0906 <122787724+Mukul0906@users.noreply.github.com>
2023-02-21 19:33:20 +0530

    Add files via upload

---
+++ Super/Delhi_Batch.java
@@ -0,0 +1,28 @@
+package feb_4th_week;
+
+class Batch { //parent class
+	//parent class property (data member/variable))
+	int id;
+	String name;
+	double fees;
+	//parent class cons
+Batch (int id,String name,double fees){
+		this.id = id;
+		this.name = name;
+		this.fees = fees;
+	}}
+class BatchMember extends Batch {
+long phone_no; // child class property
+	BatchMember(int id,String name,double fees,long phone_no){
+		super(id, name, fees); //reusing parent constructor
+		this.phone_no = phone_no;
+	}
+		void show () {
+			System.out.println(id +" "+name+" "+fees+" "+phone_no);
+			}}
+class DelhiBatch{
+	public static void main(String[] args) {
+		BatchMember obj = new BatchMember (07,"Mukul",10000,123456789);
+				obj.show();
+	}	
+}
